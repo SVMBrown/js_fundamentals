f8e726ded8eb6a78c648939f1efd3133db8c89bc
SVMBrown <scotb38@gmail.com>
2015-04-28 17:49:58 -0400

    Finished it all (decided not to repeat myself for variables.js)

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -12,7 +12,23 @@
 // cinnamon
 // cumin
 // cocoa
-
+function Recipe(title, servings, ingredients) {
+  this.title = title;
+  this.serves = servings;
+  this.ingredients = ingredients;
+  return this;
+}
+Recipe.prototype.logInfo = function() {
+  console.log(this.title);
+  console.log("serves: " + this.serves);
+  console.log("Ingredients");
+  console.log("-----------")
+  this.ingredients.forEach(function(elem) {
+    console.log(" * " + elem);
+  });
+};
+var mole = new Recipe("Mole", 2, ["cinnamon", "cumin", "cocoa"]);
+mole.logInfo();
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
@@ -20,7 +36,54 @@
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+  this.alreadyRead = false;
+  return this;
+}
+Book.prototype.read = function() {
+  this.alreadyRead = true;
+  return this;
+};
+function Library(arr) {
+  this.books = typeof(arr) === "undefined" ? [] : arr;
+  return this;
+}
+Library.prototype.addBook = function(book) {
+  if (book instanceof Book) {
+    this.books.push(book);
+  }
+}
+Library.prototype.listBooks = function() {
+  var read = [];
+  var unread = [];
+  this.books.forEach(function(elem) {
+    if(elem.alreadyRead) {
+      read.push(elem);
+    } else {
+      unread.push(elem);
+    }
+  });
+  console.log("You still have to read:");
+  unread.forEach(function(elem){
+    console.log(' * "' + elem.title + '" by: ' + elem.author + ".");
+  });
+  console.log("You have already read: ");
+  read.forEach(function(elem){
+    console.log(' * "' + elem.title + '" by: ' + elem.author + ".");
+  });
+};
 
+var myLibrary = new Library([new Book("I Wrote a Book!", "Scot")]);
+for (var i = 0; i < 5; i++) {
+  myLibrary.addBook(new Book("title" + i, "author" + i));
+  if(i % 2 === 0) {
+    myLibrary.books[i].read();
+  }
+}
+
+myLibrary.listBooks();
 // EXERCISE: The Movie Database
 
 // It's like IMDB, but much much smaller!
@@ -28,5 +91,21 @@
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
 
-
-
+function Movie(title, duration, stars) {
+  this.title = title;
+  this.duration = duration;
+  this.stars = stars;
+  return this;
+}
+Movie.prototype.printInfo = function() {
+  out = this.title + " lasts for " + this.duration + " minutes. " + "Starring: ";
+  this.stars.forEach(function(elem){
+    out += elem + ", ";
+  });
+  out = this.stars.length > 0 ? out.slice(0, out.length - 2) + "." : out + ".";
+  console.log(out);
+}
+var puff = new Movie("Puff the Magic Dragon", 30, ["Puff", "Jackie", "Living Sneezes"]);
+puff.printInfo();
+var room = new Movie("The Room", 100, ["Tommy Wiseau", "Tommy Wiseau", "Tommy Wiseau"]);
+room.printInfo();
--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -6,7 +6,7 @@
 
 // Store the following into variables: number of children, partner's name, geographic location, job title.
 // Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
-
+// SEE FUNCTIONS.JS
 // EXERCISE: The Age Calculator
 
 // Forgot how old someone is? Calculate it!
@@ -15,7 +15,7 @@
 // Store their birth year in a variable.
 // Calculate their 2 possible ages based on the stored values.
 // Output them to the screen like so: "They are either NN or NN", substituting the values.
-
+// SEE FUNCTIONS.JS
 // EXERCISE: The Lifetime Supply Calculator
 
 // Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
@@ -25,7 +25,7 @@
 // Store an estimated amount per day (as a number).
 // Calculate how many you would eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NN to last you until the ripe old age of X".
-
+// SEE FUNCTIONS.JS
 // EXERCISE: The Geometrizer
 
 // Calculate properties of a circle, using the definitions here.
@@ -33,7 +33,7 @@
 // Store a radius into a variable.
 // Calculate the circumference based on the radius, and output "The circumference is NN".
 // Calculate the area based on the radius, and output "The area is NN".
-
+// SEE FUNCTIONS.JS
 // EXERCISE: The Temperature Converter
 
 // It's hot out! Let's make a converter based on the steps here.
